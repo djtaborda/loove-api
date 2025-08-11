2466ccbaff508066d2f80ae497cbbf8624c2c84e
djtaborda <dj_afonso@hotmail.com>
2025-08-11 16:26:39 -0300

    feat(client): add Downloads page

---
+++ client/src/components/Downloads.jsx
@@ -0,0 +1,10 @@
+export default function Downloads(){
+  return (
+    <div className="container">
+      <div className="card">
+        <h3>Downloads Offline</h3>
+        <div className="meta">Gerencie suas músicas baixadas.</div>
+      </div>
+    </div>
+  );
+}
