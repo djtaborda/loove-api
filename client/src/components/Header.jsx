16f20fe2b03fe0f76e24b38b221761f35e414eaa
djtaborda <dj_afonso@hotmail.com>
2025-08-11 15:27:49 -0300

    feat(client): add Header

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,13 @@
+export default function Header({ search, setSearch }) {
+return (
+<div className="header">
+<div className="logo">Loove Music</div>
+<div className="tagline">Aqui todo mundo amo música</div>
+<div className="search-wrap">
+<input className="search" placeholder="Buscar música…"
+value={search} onChange={(e)=> setSearch(e.target.value)} />
+</div>
+</div>
+24
+);
+}
