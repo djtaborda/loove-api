d2530bb9450928b11d7cfe2ff8c33e8df34041b7
djtaborda <dj_afonso@hotmail.com>
2025-08-18 20:56:32 -0300

    Create config.js

---
+++ public/config.js
@@ -0,0 +1,4 @@
+/* SUPABASE_CONFIG_START — NÃO ALTERAR AUTOMATICAMENTE */
+window.SUPABASE_URL = 'https://sirbuhnpfesfsfvwmspx.supabase.co';   // <- troque aqui
+window.SUPABASE_ANON_KEY = 'process.env.SUPABASE_KEY';               // <- troque aqui (anon public)
+/* SUPABASE_CONFIG_END */
